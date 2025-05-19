19a2add94e12b5f3687e5182c48efeb360ecd046
ANDREY BOING <60268096+boingsolutions@users.noreply.github.com>
2025-05-19 10:06:19 -0300

    Atualização
    123

---
+++ SQL/BACKUP COMPLETO.sql
@@ -0,0 +1,11 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+-- Started on 2025-05-13 17:19:03
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
